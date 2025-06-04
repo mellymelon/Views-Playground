6f4f72d63b1be382569d1f33719bdacfff80e450
simonliang <simonliang@asl.com.hk>
2025-06-04 11:33:14 +0800

    feat: recycler view click, navigate, and pass data

--- app/src/main/java/com/example/viewsplayground/CustomAdapter.kt
+++ app/src/main/java/com/example/viewsplayground/CustomAdapter.kt
@@ -6,11 +6,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomAdapter(private val dataSet: Array<String>) :
+class CustomAdapter(private val dataSet: Array<String>, private val onClick: (String) -> Unit) :
     RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View, val onClick: (String) -> Unit) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_text_view)
+        var curStr: String? = null
+
+        init {
+            view.setOnClickListener {
+                curStr?.let { onClick(it) }
+            }
+        }
     }
 
     override fun onCreateViewHolder(
@@ -21,13 +28,14 @@ class CustomAdapter(private val dataSet: Array<String>) :
             LayoutInflater
                 .from(parent.context)
                 .inflate(R.layout.text_row_item, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view, onClick)
     }
 
     override fun onBindViewHolder(
         holder: ViewHolder,
         position: Int
     ) {
+        holder.curStr = dataSet[position]
         holder.textView.text = dataSet[position]
     }
 
--- app/src/main/java/com/example/viewsplayground/HomeFragment.kt
+++ app/src/main/java/com/example/viewsplayground/HomeFragment.kt
@@ -2,7 +2,9 @@ package com.example.viewsplayground
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,7 +15,10 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
         super.onViewCreated(view, savedInstanceState)
 
         val dataset = Array(100) { i -> "$i * $i = ${i * i}" }
-        val customAdapter = CustomAdapter(dataset)
+        val customAdapter = CustomAdapter(dataset) { str ->
+            val bundle = bundleOf("detail_string" to str)
+            findNavController().navigate(R.id.navigate_to_detail, bundle)
+        }
         val recyclerView = view.findViewById<RecyclerView>(R.id.numberList)
         recyclerView.adapter = customAdapter
         recyclerView.addItemDecoration(
@@ -22,22 +27,5 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
                 DividerItemDecoration.VERTICAL
             )
         )
-//        ViewCompat.setOnApplyWindowInsetsListener(recyclerView) { v, insets ->
-//            val innerPadding = insets.getInsets(
-//                WindowInsetsCompat.Type.systemBars()
-//                        or WindowInsetsCompat.Type.displayCutout()
-//                // If using EditText, also add
-//                // "or WindowInsetsCompat.Type.ime()" to
-//                // maintain focus when opening the IME
-//            )
-//            Log.d(TAG, "padding分别是$innerPadding")
-//            v.setPadding(
-//                innerPadding.left,
-//                innerPadding.top,
-//                innerPadding.right,
-//                innerPadding.bottom
-//            )
-//            insets //或者WindowInsetsCompat.CONSUMED（如果不希望影响子元素）
-//        }
     }
 }
